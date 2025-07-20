fdd1512db60f947965ca099b2561bc6f235f265c
NagabhushanBaduka <155204487+NagabhushanBaduka@users.noreply.github.com>
2025-07-20 09:32:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes import products, orders
 from database import init_db
+import uvicorn
+import os
 
 app = FastAPI(title="E-commerce API")
 
@@ -10,3 +12,6 @@ app.include_router(orders.router, prefix="/orders", tags=["Orders"])
 @app.on_event("startup")
 async def startup_event():
     await init_db()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  
+    uvicorn.run("main:app", host="0.0.0.0", port=port)    
